72555a28cd2643bef2d35cd808adf4c9ab78535e
mustafazdl <gm.mustafaozdil@gmail.com>
2022-11-03 22:45:26 +0300

    B01

--- src/A04.java
+++ src/A04.java
@@ -1,5 +1,17 @@
 public class A04 {
     public static void main(String[] args) {
 
+        /*
+        Bir projeyi ilk defa oluşturduğumuzda;
+        1-commit oluştur
+        2-uzak masaüstünde repo olustur.
+        3-github'da istenen son 3 satiri copy yapip projeye paste yapip calistir.
+
+        Bir projeyi uzak masaüstü ile ilişkilendirilince sonra yapaxağımız her değişiklik için
+        1-git add . : yeni yapilan değişiklikleri git'e ekler.
+        2-git commit -m"istenen açıklama" : değişiklikleri işleyerek versiyon oluşturur.
+        3- git push : son versiyonu uzak masaüstüne yollar.
+         */
+
     }
 }
