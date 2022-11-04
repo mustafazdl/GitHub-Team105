04c49a01a4b054c2df56df2cd4123d85feaf35de
mustafazdl <gm.mustafaozdil@gmail.com>
2022-11-05 00:15:40 +0300

    deneme branch yeni degisiklik

--- src/E01.java
+++ src/E01.java
@@ -1,10 +1,10 @@
 public class E01 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("main branch'inde degisiklik yaptım");
-=======
-        System.out.println("deneme branc degisiklik yaptim");
->>>>>>> deneme
 
+        System.out.println("main branch'inde degisiklik yaptım");
+
+        System.out.println("deneme branc degisiklik yaptim");
+
+        System.out.println("deneme branch e01 8.satr degisiklik");
     }
 }
