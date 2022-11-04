35b5c2282b568c5b4d02b0e982833b6f5e5412c4
mustafazdl <gm.mustafaozdil@gmail.com>
2022-11-05 00:02:57 +0300

    main branch E01 degisiklik

--- src/E01.java
+++ src/E01.java
@@ -1,6 +1,6 @@
 public class E01 {
     public static void main(String[] args) {
-        System.out.println("deneme branch'inde degisiklik yaptım");
+        System.out.println("main branch'inde degisiklik yaptım");
 
     }
 }
